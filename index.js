aed87ea99cb701cbdc4060315d0094b42f1b23a2
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-06-28 15:04:53 +0000

    add styles to css file

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function setColor(name) {
     body.style.backgroundColor = name;
 }
 
+function randomColor() {
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color;
+}
